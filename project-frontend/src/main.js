13a96867acf12c7bb83374183262ade9f131b546
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-05-28 00:31:57 +0200

    main.js aktualisieren

--- project-frontend/src/main.js
+++ project-frontend/src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
 import router from './router';
+import api from './services/api';
 
 const app = createApp(App);
+
+app.config.globalProperties.$http = api; // Register Axios globally
 
 app.use(store);
 app.use(router);
