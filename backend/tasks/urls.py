6493418e6a62fa8ef241e686add3dcde673dd743
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-05-27 02:28:16 +0200

    urls.py aktualisieren

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
+from .views import TaskViewSet, TimeEntryViewSet
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
+router.register(r'time-entries', TimeEntryViewSet)
+
+urlpatterns = router.urls
+
+
 
 urlpatterns = [
     path('', include(router.urls)),
