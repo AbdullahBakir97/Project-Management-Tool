c0a54c4e4e2b549c7be2ac56e3b819b06df0ccd9
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-05-30 01:49:14 +0200

    Add team members permission

--- backend/tasks/permissions.py
+++ backend/tasks/permissions.py
@@ -11,3 +11,16 @@ class IsProjectManagerOrReadOnly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         return request.user.role in ['admin', 'manager']
+
+# Permission for Members to access their own tasks or Read-Only
+class IsMemberOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        if request.user.is_authenticated and request.user.role == 'member':
+            # Additional check to see if the member is only updating their own tasks
+            if view.action in ['update', 'partial_update', 'destroy']:
+                task_id = view.kwargs.get('pk')
+                return request.user.tasks.filter(id=task_id).exists()
+            return True
+        return False
